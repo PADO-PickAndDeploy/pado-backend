f743519f194635f9684d76808abf43c015a3b97d
Kim-Taehyeong <taehyeok02@gmail.com>
2025-08-05 00:28:25 +0900

    refactor : Folder Name Renameing pado-server -> api-server

--- api-server/src/main/java/org/pado/api/PadoServerApplication.java
+++ api-server/src/main/java/org/pado/api/PadoServerApplication.java
@@ -1,4 +1,4 @@
-package org.pado.server;
+package org.pado.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- api-server/src/test/java/org/pado/api/PadoServerApplicationTests.java
+++ api-server/src/test/java/org/pado/api/PadoServerApplicationTests.java
@@ -1,4 +1,4 @@
-package org.pado.pado_server;
+package org.pado.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
