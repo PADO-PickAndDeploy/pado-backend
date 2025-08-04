c7a4891bf7023e6b8fcabfb62e51b97d47c24cde
Kim-Taehyeong <taehyeok02@gmail.com>
2025-08-05 00:18:13 +0900

    feat: Init Spring Project

---
+++ pado-server/src/main/java/org/pado/server/PadoServerApplication.java
@@ -0,0 +1,13 @@
+package org.pado.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PadoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PadoServerApplication.class, args);
+	}
+
+}
---
+++ pado-server/src/test/java/org/pado/pado_server/PadoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pado.pado_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PadoServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
