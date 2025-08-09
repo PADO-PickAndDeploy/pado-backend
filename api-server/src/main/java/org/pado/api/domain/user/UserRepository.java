037c610c0b5e85f70f7144f932057bd70ee828b0
thrkrif <103479629+thrkrif@users.noreply.github.com>
2025-08-09 19:52:18 +0900

    refactor: Apply Optional return types to Repository methods

--- api-server/src/main/java/org/pado/api/domain/user/UserRepository.java
+++ api-server/src/main/java/org/pado/api/domain/user/UserRepository.java
@@ -1,10 +1,12 @@
 package org.pado.api.domain.user;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByName(String name);
-    User findByEmail(String email);
+    Optional<User> findByName(String name);
+    Optional<User> findByEmail(String email);
     boolean existsByName(String name);
     boolean existsByEmail(String email);
 }
