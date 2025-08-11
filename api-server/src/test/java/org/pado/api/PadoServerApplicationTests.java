879562b5d9fdc1a18d1b5750fc1ad28cdd0885ef
thrkrif <103479629+thrkrif@users.noreply.github.com>
2025-08-11 22:34:20 +0900

    debug: Add detailed logging and clean build to CI

--- api-server/src/test/java/org/pado/api/PadoServerApplicationTests.java
+++ api-server/src/test/java/org/pado/api/PadoServerApplicationTests.java
@@ -10,16 +10,6 @@ class PadoServerApplicationTests {
 
 	@Test
 	void contextLoads() {
-		try {
-			System.out.println("=== 컨텍스트 로딩 시작 ===");
-			// 컨텍스트 로딩 완료되면 여기까지 옴
-			System.out.println("=== 컨텍스트 로딩 성공 ===");
-		} catch (Exception e) {
-			System.err.println("=== 에러 발생 ===");
-			System.err.println("Error: " + e.getClass().getSimpleName());
-			System.err.println("Message: " + e.getMessage());
-			e.printStackTrace();
-			throw e; // 다시 던져서 테스트 실패하게 함
-		}
+		
 	}
 }
