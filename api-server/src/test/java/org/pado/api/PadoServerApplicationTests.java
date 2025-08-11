07d1319d1d672ce653132a77e5ec7cfc8fb62f37
thrkrif <103479629+thrkrif@users.noreply.github.com>
2025-08-12 01:37:53 +0900

    refactor: Add Redis configuration for CI environment

--- api-server/src/test/java/org/pado/api/PadoServerApplicationTests.java
+++ api-server/src/test/java/org/pado/api/PadoServerApplicationTests.java
@@ -1,15 +1,11 @@
 package org.pado.api;
 
 import org.junit.jupiter.api.Test;
-import org.pado.api.config.TestRedisConfig;
-import org.pado.api.config.TestSecurityConfig;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 @ActiveProfiles("ci")
-@Import({TestSecurityConfig.class, TestRedisConfig.class})
 class PadoServerApplicationTests {
 
 	@Test
