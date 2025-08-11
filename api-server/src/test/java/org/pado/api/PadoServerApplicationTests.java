14200a1e7846766fa7098d9e21ca4892b329af16
thrkrif <103479629+thrkrif@users.noreply.github.com>
2025-08-11 21:38:55 +0900

    refactor: Remove test configuration and add debug logging

--- api-server/src/test/java/org/pado/api/PadoServerApplicationTests.java
+++ api-server/src/test/java/org/pado/api/PadoServerApplicationTests.java
@@ -1,42 +1,24 @@
 package org.pado.api;
 
 import org.junit.jupiter.api.Test;
-import org.pado.api.core.security.jwt.TokenBlacklistService;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.Primary;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@SpringBootTest(
+	webEnvironment = SpringBootTest.WebEnvironment.NONE,
+	// 로그 확인 용으로 추가
+	properties = {
+        "logging.level.org.springframework.beans.factory=DEBUG",
+        "logging.level.org.springframework.context=DEBUG",
+        "logging.level.org.springframework.boot.autoconfigure=DEBUG",
+        "logging.level.org.pado=DEBUG"
+    }
+)
 @ActiveProfiles("ci")
-@Import(PadoServerApplicationTests.TestConfigForCI.class)
 class PadoServerApplicationTests {
 
 	@Test
 	void contextLoads() {
 		// 컨텍스트 로딩 테스트
 	}
-
-	@TestConfiguration
-	static class TestConfigForCI {
-
-		@Bean
-		@Primary
-		public TokenBlacklistService testTokenBlacklistService() {
-			return new TokenBlacklistService() {
-				@Override
-				public void blacklistToken(String token) {
-					// NoOp implementation for CI
-				}
-
-				@Override
-				public boolean isBlacklisted(String token) {
-					return false;
-				}
-			};
-		}
-	}
-
 }
