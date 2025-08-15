dcbb73e853380bc85f6de705344675607380cd6a
thrkrif <103479629+thrkrif@users.noreply.github.com>
2025-08-16 04:11:19 +0900

    feat: implement credential getAll

--- api-server/src/main/java/org/pado/api/domain/credential/CredentialRepository.java
+++ api-server/src/main/java/org/pado/api/domain/credential/CredentialRepository.java
@@ -2,9 +2,11 @@ package org.pado.api.domain.credential;
 
 import java.util.List;
 
+import org.pado.api.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CredentialRepository extends JpaRepository<Credential, Long> {
+    List<Credential> findByUser(User user);
     List<Credential> findByUserId(Long userId);
     List<Credential> findByName(String name);    
     List<Credential> findByType(String type);
